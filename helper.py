25c8d4f7d26f214bef2fc3b5f0f38428a103b186
Pankil Kalra <56042783+pankilkalra@users.noreply.github.com>
2019-11-07 01:48:59 +0530

    Update helper.py

--- helper.py
+++ helper.py
@@ -10,18 +10,11 @@ def decimal_to_binary(n):
 
 
 def convert_twos_complement(n):
-	f=0
-	if(n<0):
-		f=1
-		n=-n
-	n=decimal_to_binary(n)
-	print(n)
-	if f==1:
-		n=list(n)
-		for i in range(len(n)):
-			n[i]=str(1-int(n[i]))
-		n="".join(n)
-		n=add(n,decimal_to_binary(1))
+	n=list(n)
+	for i in range(len(n)):
+		n[i]=str(1-int(n[i]))
+	n="".join(n)
+	n=add(n,decimal_to_binary(1))
 	return n	
 	
 
@@ -36,5 +29,6 @@ def add(n1, n2):
 			c=1
 		else:
 			c=0
-	return ans		
+	return ans							
+
 
