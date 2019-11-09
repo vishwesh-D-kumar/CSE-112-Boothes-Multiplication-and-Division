b80ba4f9568b4daa5085a1e8d50827b682ee05d3
Pankil Kalra <56042783+pankilkalra@users.noreply.github.com>
2019-11-09 16:34:56 +0530

    Update division.py

--- division.py
+++ division.py
@@ -6,6 +6,14 @@ def boothes(dividend,divisor):
 		return
 	aplusq=convert_twos_complement(dividend,22)
 	m=convert_twos_complement(divisor,11)
+	flag1=0
+	flag2=0
+	if aplusq[0]=="1":
+		aplusq=twos_complement(aplusq,22)
+		flag1=1
+	if m[0]=="1":
+		m=twos_complement(m,11)
+		flag2=1				
 	a=aplusq[:11]
 	q=aplusq[11:]
 	count=11
@@ -24,16 +32,18 @@ def boothes(dividend,divisor):
 			q=q[:-1]+"1"
 		else:
 			a=previous
-		print("Step:",0,"A:",a,"Q:",q,"M:",m)
+		print("Step:",i+1,"A:",a,"Q:",q,"M:",m)
 
-
-	if dividend*divisor<0:
+	if flag1==1 and flag2==0:
 		q=twos_complement(q,11)
+		a=twos_complement(a,11)
+	if flag1==0 and flag2==1:
+		q=twos_complement(q,11)
+	if flag1==1 and flag2==1:
+		a=twos_complement(a,11)
 	a=twos_complement_to_decimal(a)
 	q=twos_complement_to_decimal(q)	
 	print("Quotient:",q,"Remainder:",a)
-
-
 
 print("Enter Dividend : ")
 dividend=int(input())
